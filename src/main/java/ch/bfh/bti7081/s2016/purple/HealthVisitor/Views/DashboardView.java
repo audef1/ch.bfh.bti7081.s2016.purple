cd97c2e923988469b2a85df52891fe41bc85d384
Florian Auderset <f_auderset@hotmail.com>
2016-05-20 17:23:45 +0200

    adding views package

---
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/Views/DashboardView.java
@@ -0,0 +1,34 @@
+package ch.bfh.bti7081.s2016.purple.HealthVisitor.Views;
+
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.VerticalLayout;
+
+public class DashboardView extends VerticalLayout implements View {
+
+	public DashboardView() {
+            	
+    	setSizeFull();
+        
+        Button button = new Button("Go to Main View",
+                new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+				navigator.navigateTo(MAINVIEW);
+            }
+
+        });
+        addComponent(button);
+        setComponentAlignment(button, Alignment.MIDDLE_CENTER);
+    }
+
+    @Override
+    public void enter(ViewChangeEvent event) {
+        Notification.show("Welcome to the Animal Farm");
+    }
+}

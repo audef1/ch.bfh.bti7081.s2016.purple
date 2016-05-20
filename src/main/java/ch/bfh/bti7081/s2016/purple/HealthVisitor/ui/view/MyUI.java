69dfad59f291237a754136340eaf1f5634f81ef9
Tobias Flühmann <t.fluehmann94@hotmail.com>
2016-05-20 19:58:08 +0200

    refactored packages according to https://vaadin.com/wiki/-/wiki/Main/MVC+Basics+in+Vaadin

---
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/view/MyUI.java
@@ -0,0 +1,60 @@
+package ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.view;
+
+import ch.bfh.bti7081.s2016.purple.HealthVisitor.database.TestEntity;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add CustomComp to the user interface and initialize non-CustomComp functionality.
+ */
+@Theme("mytheme")
+@Widgetset("ch.bfh.bti7081.s2016.purple.HealthVisitor.MyAppWidgetset")
+public class MyUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        final TextField name = new TextField();
+        name.setCaption("Type your name here:");
+
+        Button button = new Button("Click Me");
+        button.addClickListener( e -> {
+            layout.addComponent(new Label("Thanks " + name.getValue() 
+                    + ", it works!"));
+        });
+        
+        layout.addComponents(name, button);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        
+        setContent(layout);
+
+        EntityManagerFactory emFac = Persistence.createEntityManagerFactory("EclipseLink_JPA");
+        EntityManager em = emFac.createEntityManager();
+        em.getTransaction().begin();
+        TestEntity test = new TestEntity(1, "hello");
+        em.persist(test);
+        System.out.println("Persisted");
+        em.close();
+        emFac.close();
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}

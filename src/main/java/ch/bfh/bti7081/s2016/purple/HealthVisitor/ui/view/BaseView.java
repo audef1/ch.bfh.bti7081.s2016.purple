7825b517a774a0a338a60958fe5a00ad89b08675
Raphael Suter <raphael.suter@students.bfh.ch>
2016-05-25 21:37:11 +0200

    modified base view

--- src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/view/BaseView.java
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/view/BaseView.java
@@ -1,30 +1,29 @@
 package ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.view;
 
+import com.vaadin.navigator.View;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Layout;
+import com.vaadin.ui.VerticalLayout;
 
-import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.HealthVisitorEntity;
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.component.FooterComponent;
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.component.HeaderComponent;
-import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.controller.BaseController;
-import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.controller.DashboardController;
-import com.vaadin.navigator.View;
-import com.vaadin.server.VaadinSession;
-import com.vaadin.ui.*;
 
 /**
  * Created by tgdflto1 on 25/05/16.
  */
 public abstract class BaseView extends CustomComponent implements View {
-    protected VerticalLayout vl;
+	protected VerticalLayout vl;
 
-    public BaseView() {
-        vl = new VerticalLayout();
-        vl.setWidth("100%");
-        vl.addComponent(new HeaderComponent()); //Header
-        vl.addComponent(initView()); //Custom content
-        vl.addComponent(new FooterComponent()); //Footer
-        setCompositionRoot(vl);
+	public BaseView() {
+		vl = new VerticalLayout();
+		vl.setSizeFull();
+		vl.addComponent(new HeaderComponent()); // Header
+		vl.addComponent(initView()); // Custom content
+		vl.addComponent(new FooterComponent()); // Footer
+		setCompositionRoot(vl);
 
-    }
+	}
 
-    protected abstract Layout initView();
+	protected abstract Layout initView();
+
 }

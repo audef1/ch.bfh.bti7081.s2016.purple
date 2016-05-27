3d452248aea54e60963db8a5999d8645d529542a
Tobias Flühmann <t.fluehmann94@hotmail.com>
2016-05-27 15:47:48 +0200

    merge broke before

--- src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/component/HeaderComponent.java
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/component/HeaderComponent.java
@@ -1,5 +1,6 @@
 package ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.component;
 
+import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.view.BaseView;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.shared.ui.AlignmentInfo;
 import com.vaadin.shared.ui.MarginInfo;
@@ -16,28 +17,31 @@ public class HeaderComponent extends HorizontalLayout {
     private HorizontalLayout buttons;
 
 
-    public HeaderComponent(String pageName){
-       
-    	logout =  new LogoutButton();
-    	pagename = new Label(pageName);
-    	logo = new Image("Logo", new ThemeResource("images/Logo_HealthVisitor.png"));
-    	buttons = new HorizontalLayout();
-    	
-    	this.setWidth("100%");
+    public HeaderComponent(BaseView view) {
+
+        logout = new LogoutButton();
+        pagename = new Label(view.getViewName());
+        logo = new Image("Logo", new ThemeResource("images/Logo_HealthVisitor.png"));
+        buttons = new HorizontalLayout();
+
+        this.setWidth("100%");
         this.setSizeFull();
         this.setMargin(new MarginInfo(false, true, false, false));
-        
+
         logo.setWidth("300px");
         logo.setCaption("");
         pagename.addStyleName("pagename");
         buttons.addComponent(logout);
         buttons.setComponentAlignment(logout, Alignment.MIDDLE_LEFT);
-        
+
         this.addComponent(pagename);
         this.addComponent(buttons);
         this.addComponent(logo);
         this.setComponentAlignment(pagename, Alignment.MIDDLE_LEFT);
         this.setComponentAlignment(buttons, Alignment.MIDDLE_CENTER);
         this.setComponentAlignment(logo, Alignment.MIDDLE_RIGHT);
+
     }
+
+
 }

ab2dc7fe54645e687e151f4d8231b29161704b54
Florian Auderset <f_auderset@hotmail.com>
2016-05-29 10:25:05 +0200

    added home and fake back button to header

--- src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/component/HeaderComponent.java
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/component/HeaderComponent.java
@@ -1,6 +1,8 @@
 package ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.component;
 
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.view.BaseView;
+
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.shared.ui.AlignmentInfo;
 import com.vaadin.shared.ui.MarginInfo;
@@ -12,18 +14,23 @@ import com.vaadin.ui.*;
 public class HeaderComponent extends HorizontalLayout {
 
     private Label pagename;
-    private LogoutButton logout;
+    private NavigationButton logout;
+    private NavigationButton back;
+    private NavigationButton home;
     private Image logo;
     private HorizontalLayout buttons;
 
 
     public HeaderComponent(BaseView view) {
 
-        logout = new LogoutButton();
+        logout = new NavigationButton("Logout", "Logout", FontAwesome.SIGN_OUT);
+        back = new NavigationButton("zurück", "Dashboard", FontAwesome.BACKWARD);
+        home = new NavigationButton("", "Dashboard", FontAwesome.HOME);
+
         pagename = new Label(view.getViewName());
         logo = new Image("Logo", new ThemeResource("images/Logo_HealthVisitor.png"));
         buttons = new HorizontalLayout();
-
+        
         this.setWidth("100%");
         this.setSizeFull();
         this.setMargin(new MarginInfo(false, true, false, false));
@@ -31,7 +38,10 @@ public class HeaderComponent extends HorizontalLayout {
         logo.setWidth("300px");
         logo.setCaption("");
         pagename.addStyleName("pagename");
+        buttons.addComponent(home);
+        buttons.addComponent(back);
         buttons.addComponent(logout);
+        buttons.setSpacing(true);
         buttons.setComponentAlignment(logout, Alignment.MIDDLE_LEFT);
 
         this.addComponent(pagename);
---
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/component/NavigationButton.java
@@ -0,0 +1,14 @@
+package ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.component;
+
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Button;
+
+public class NavigationButton extends Button {
+
+	  public NavigationButton(String name, String view, FontAwesome icon){
+	        super(name);
+	        this.setIcon(icon);
+	        this.setData(name);
+	        this.addClickListener(click -> {getUI().getNavigator().navigateTo(view);});
+	    }
+}

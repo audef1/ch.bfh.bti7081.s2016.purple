62d38b3303122aaa55ea7503a6db5d090f97d583
Tobias Flühmann <t.fluehmann94@hotmail.com>
2016-05-29 13:52:39 +0200

    added report entity

---
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/data/ReportEntity.java
@@ -0,0 +1,47 @@
+package ch.bfh.bti7081.s2016.purple.HealthVisitor.data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by tgdflto1 on 29/05/16.
+ */
+@Entity(name="report")
+@Table(name="REPORT")
+public class ReportEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="APPOINTMENT_ID")
+    private  AppointmentEntity appointment;
+
+    private Date start;
+    private Date end;
+    private String description;
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+}

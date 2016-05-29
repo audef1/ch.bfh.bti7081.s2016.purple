7169d7b424de6fba348efccf38065d2da1bf1a9a
Florian Auderset <f_auderset@hotmail.com>
2016-05-29 12:15:45 +0200

    data from container

--- src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/view/AppointmentListView.java
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/view/AppointmentListView.java
@@ -1,5 +1,7 @@
 package ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.view;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.AppointmentEntity;
@@ -12,10 +14,12 @@ import com.vaadin.ui.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.components.calendar.event.BasicEvent;
 
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.controller.AppointmentListController;
 
@@ -25,10 +29,14 @@ import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.controller.AppointmentListCo
 public class AppointmentListView extends BaseView {
 	public static final String NAME = "AppointmentList";
 	public static final String VIEW_NAME = "Terminliste";
+	public static final int FIRST_HOUR = 0;
+	public static final int LAST_HOUR = 24;
 
 	private static final Logger logger = LogManager.getLogger(AppointmentListView.class);
 	private final AppointmentListController controller;
 	private VerticalLayout general;
+	
+	private BeanItemContainer<BasicEvent> container;
 
 	public AppointmentListView() {
 		super();
@@ -50,9 +58,9 @@ public class AppointmentListView extends BaseView {
 
 		HealthVisitorEntity user = controller.getUser();
 		List<AppointmentEntity> items = user.getAppointments();
-		
-		//final BeanItemContainer<AppointmentEntity> container = new BeanItemContainer<AppointmentEntity>(AppointmentEntity.class, items);
-		
+		container = new BeanItemContainer<>(BasicEvent.class);
+		this.addAppointments(items);
+				
 		HorizontalLayout calnav = new HorizontalLayout();
 		calnav.setSpacing(true);
 		Button week = new Button("Woche");
@@ -62,12 +70,32 @@ public class AppointmentListView extends BaseView {
 		Calendar cal = new Calendar(new AppointmentEventProvider(items));
 		cal.setSizeFull();
 		cal.setResponsive(true);
-		cal.setFirstVisibleHourOfDay(cal.getFirstVisibleHourOfDay());
+		cal.setFirstVisibleHourOfDay(FIRST_HOUR);
+	    cal.setLastVisibleHourOfDay(LAST_HOUR);
+	    cal.setContainerDataSource(container);
+		
 		general.addComponents(calnav, cal);
 		
 		// TODO Add patients and appointments from db to the list
 		//addSelectionListener((clickEvent -> getUI().getNavigator().navigateTo(AppointmentDetailView.NAME)));
 		return general;
+	}
+	
+	public void addAppointments(List<AppointmentEntity> items) {
+        for (AppointmentEntity appointment : items) {
+        	String name = appointment.getClient().getFirstName() + " " + appointment.getClient().getLastName();
+        	String description = appointment.getAddress();
+        	
+        	GregorianCalendar cal = new GregorianCalendar();
+			cal.setTime(new Date(appointment.getStartTime()));
+			Date start = cal.getTime();
+			
+			cal.add(GregorianCalendar.MINUTE, appointment.getDuration());
+			Date end = cal.getTime();
+
+            container.addBean(new BasicEvent(name, description, start, end));
+        }
+        container.sort(new Object[]{"start"}, new boolean[] { true });
 	}
 
 	@Override

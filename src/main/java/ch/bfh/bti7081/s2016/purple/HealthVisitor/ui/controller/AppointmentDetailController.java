a87b5fb4079771a562e98741265f46afc4be01a3
Tobias Flühmann <t.fluehmann94@hotmail.com>
2016-05-29 22:00:57 +0200

    details for client can be changed

--- src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/controller/AppointmentDetailController.java
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/controller/AppointmentDetailController.java
@@ -1,10 +1,32 @@
 package ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.controller;
 
+import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.AppointmentEntity;
+import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.ClientEntity;
+import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.businesslogic.AppointmentDao;
+import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.businesslogic.ClientDao;
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.view.AppointmentDetailView;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.TextArea;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AppointmentDetailController extends BaseController {
+	private static final Logger logger = LogManager.getLogger(AppointmentDetailController.class);
 
 	public AppointmentDetailController(AppointmentDetailView view){
 		super(view);
+	}
+
+	public AppointmentEntity getCurrentAppointment() {
+		return new AppointmentDao().getCurrentAppointment();
+	}
+
+	public void saveDetails(Button button, AppointmentEntity appointmentEntity, String description) {
+		ClientEntity client = appointmentEntity.getClient();
+		logger.debug("setting details for " + client.getFullName() + " to: "+description);
+		client.setDetails(description);
+		new ClientDao().persist(client);
+		button.setCaption("saved");
+		button.setEnabled(false);
 	}
 }
--- src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/view/AppointmentDetailView.java
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/view/AppointmentDetailView.java
@@ -1,10 +1,13 @@
 package ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.view;
 
+import java.util.Date;
 import java.util.Locale;
 
+import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.AppointmentEntity;
 import com.vaadin.data.validator.DateRangeValidator;
 import com.vaadin.data.validator.StringLengthValidator;
 import com.vaadin.shared.ui.datefield.Resolution;
+import com.vaadin.ui.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,22 +17,10 @@ import com.vaadin.server.FontAwesome;
 import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.tapio.googlemaps.GoogleMap;
 import com.vaadin.tapio.googlemaps.client.LatLon;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.DateField;
-import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.Grid;
-import com.vaadin.ui.GridLayout;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Layout;
-import com.vaadin.ui.RichTextArea;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Window;
 
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.component.StandardLayout;
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.controller.AppointmentDetailController;
+import org.eclipse.persistence.jpa.jpql.parser.DateTime;
 
 public class AppointmentDetailView extends BaseView{
 	public static final String NAME = "AppointmentDetail";
@@ -52,113 +43,129 @@ public class AppointmentDetailView extends BaseView{
 		general.setMargin(new MarginInfo(false, false, true, true));
 		general.setSpacing(true);
 
+		AppointmentEntity appointment = controller.getCurrentAppointment();
+		if(appointment == null){
+			general.addComponent(new Label("Heute keinen Termin gefunden"));
+		}else{
 
-		Label lblHeader = new Label("29.02.2016, 15:30-17:00 - Wolfgang Schaefer");
-		lblHeader.setStyleName("h1");
 
-		Button btnArrival = new Button("Ankunft bestätigen");
-		btnArrival.setWidth("200px");
+			Label lblHeader = new Label(appointment.getDate() +", " + appointment.getStartTime() +
+					"-"+appointment.getEndTime()+" - "+appointment.getClient().getFullName());
+			lblHeader.setStyleName("h1");
 
-//		Google-Maps-Implementation
-		LatLon pos = new LatLon(46.9648208, 7.453848);
+			Button btnArrival = new Button("Ankunft bestätigen");
+			btnArrival.setWidth("200px");
 
-		GoogleMap map = new GoogleMap("AIzaSyCnqIoyh9ULI3b6rtkCYXPdMXRqivaH714", null, "german");
-		map.setSizeFull();
-		map.addMarker("Wankdorffeldstrasse 102, 3014 Bern", pos, false, null);
-		map.setMinZoom(4);
-		map.setMaxZoom(16);
-		map.setCenter(pos);
-		map.setZoom(16);
+	//		Google-Maps-Implementation
+			LatLon pos = new LatLon(46.9648208, 7.453848);
 
-//		Create Checklist
-		Grid checklist = new Grid();
-		checklist.setColumns("Aufgabenliste");
+			GoogleMap map = new GoogleMap("AIzaSyCnqIoyh9ULI3b6rtkCYXPdMXRqivaH714", null, "german");
+			map.setSizeFull();
+			map.addMarker("Wankdorffeldstrasse 102, 3014 Bern", pos, false, null);
+			map.setMinZoom(4);
+			map.setMaxZoom(16);
+			map.setCenter(pos);
+			map.setZoom(16);
 
-//		Create Column last report
-		Label lastReport = new Label("Letzter Rapport");
-		lastReport.setStyleName("h2");
+	//		Create Checklist
+			Grid checklist = new Grid();
+			checklist.setColumns("Aufgabenliste");
 
-//		Show the last report
-		Label report = new Label("Hier steht der letzte Rapport.");
+	//		Create Column last report
+			Label lastReport = new Label("Letzter Rapport");
+			lastReport.setStyleName("h2");
 
-//		Create button to show the form of the new report 
-		Button btnNewReport = new Button("Neuer Rapport erfassen");
-		if (btnArrival.getCaption().equals("Ende bestätigen")){
-			btnNewReport.setEnabled(true);
-		} else {
-			btnNewReport.setEnabled(false);
+	//		Show the last report
+			Label report = new Label("Hier steht der letzte Rapport.");
+
+	//		Create button to show the form of the new report
+			Button btnNewReport = new Button("Neuer Rapport erfassen");
+			if (btnArrival.getCaption().equals("Ende bestätigen")){
+				btnNewReport.setEnabled(true);
+			} else {
+				btnNewReport.setEnabled(false);
+			}
+
+
+	//		Add clicklistener to button Arrival
+			btnArrival.addClickListener(clickevent ->
+			btnArrivalClicked(btnArrival, btnNewReport));
+
+	//		Create button to show patient details
+			Button btnDetails = new Button("Patientendetails");
+	//		TODO: Show Patient data
+
+
+	//		Create Column Info and emergency-contact
+			Label lblBeschriebTitle = new Label("Kurzbeschrieb");
+			lblBeschriebTitle.setStyleName("h2");
+			Button saveClientDetails = new Button("Details speichern");
+			saveClientDetails.setEnabled(false);
+
+	//		Show short description about the patient
+			TextArea description = new TextArea();
+			description.setValue(appointment.getClient().getDetails());
+			description.addTextChangeListener(click -> {saveClientDetails.setCaption("Speichern"); saveClientDetails.setEnabled(true);});
+
+			saveClientDetails.addClickListener(click -> controller.saveDetails(saveClientDetails, appointment, description.getValue()));
+			Button btnEmergencyContact = new Button("Notfallkontakte des Patienten");
+			VerticalLayout patientDetails = new VerticalLayout(description, saveClientDetails);
+
+	//		TODO: EmerencyContactView
+			//		btnEmergencyContact.addClickListener(clickevent -> getUI().getNavigator().navigateTo(emergencyContactView.NAME));
+
+
+			// The Layout for title and arrival button
+			GridLayout top = new GridLayout(2, 2);
+			top.setColumnExpandRatio(0, 10);
+			top.setColumnExpandRatio(1, 0);
+			top.setSizeFull();
+			top.addComponent(lblHeader, 0, 0);
+			top.setComponentAlignment(lblHeader, Alignment.MIDDLE_LEFT);
+			top.addComponent(btnArrival, 1, 0);
+			top.setComponentAlignment(btnArrival, Alignment.MIDDLE_RIGHT);
+			top.setMargin(new MarginInfo(true, true, false, false));
+
+
+	//		Set the data-Layout
+			GridLayout data = new GridLayout(3, 5);
+			data.setSpacing(true);
+			data.setMargin(false);
+			data.setSizeFull();
+
+			data.addComponent(map, 0, 0, 0, 2);
+			data.addComponent(checklist, 0, 3, 0, 4);
+			data.addComponent(lastReport, 1, 0);
+			data.setComponentAlignment(lastReport, Alignment.TOP_LEFT);
+			data.addComponent(report, 1, 1, 1, 2);
+			data.setComponentAlignment(report, Alignment.TOP_LEFT);
+			data.addComponent(btnNewReport, 1, 3);
+			data.setComponentAlignment(btnNewReport, Alignment.TOP_LEFT);
+			data.addComponent(btnDetails, 1, 4);
+			data.setComponentAlignment(btnDetails, Alignment.TOP_LEFT);
+			data.addComponent(lblBeschriebTitle, 2, 0);
+			data.setComponentAlignment(lblBeschriebTitle, Alignment.TOP_LEFT);
+			data.addComponent(patientDetails, 2, 1, 2, 3);
+			data.setComponentAlignment(patientDetails, Alignment.TOP_LEFT);
+			data.addComponent(btnEmergencyContact, 2, 4);
+			data.setComponentAlignment(btnEmergencyContact, Alignment.TOP_LEFT);
+
+	//		data.addComponents(map, checklist);
+
+
+
+
+			// Set the root layout
+			general.addComponent(top);
+			general.addComponent(data);
 		}
-		
-
-//		Add clicklistener to button Arrival
-		btnArrival.addClickListener(clickevent ->
-		btnArrivalClicked(btnArrival, btnNewReport));
-
-//		Create button to show patient details
-		Button btnDetails = new Button("Patientendetails");
-//		TODO: Show Patient data
-
-
-//		Create Column Info and emergency-contact
-		Label lblBeschriebTitle = new Label("Kurzbeschrieb");
-		lblBeschriebTitle.setStyleName("h2");
-
-//		Show short description about the patient
-		Label lblBeschrieb = new Label("Hier steht der Kurzbeschrieb über den Patienten");
-
-		Button btnEmergencyContact = new Button("Notfallkontakte des Patienten");
-//		TODO: EmerencyContactView
-		//		btnEmergencyContact.addClickListener(clickevent -> getUI().getNavigator().navigateTo(emergencyContactView.NAME));
-
-
-		// The Layout for title and arrival button
-		GridLayout top = new GridLayout(2, 2);
-		top.setColumnExpandRatio(0, 10);
-		top.setColumnExpandRatio(1, 0);
-		top.setSizeFull();
-		top.addComponent(lblHeader, 0, 0);
-		top.setComponentAlignment(lblHeader, Alignment.MIDDLE_LEFT);
-		top.addComponent(btnArrival, 1, 0);
-		top.setComponentAlignment(btnArrival, Alignment.MIDDLE_RIGHT);
-		top.setMargin(new MarginInfo(true, true, false, false));
-
-
-//		Set the data-Layout
-		GridLayout data = new GridLayout(3, 5);
-		data.setSpacing(true);
-		data.setMargin(false);
-		data.setSizeFull();
-
-		data.addComponent(map, 0, 0, 0, 2);
-		data.addComponent(checklist, 0, 3, 0, 4);
-		data.addComponent(lastReport, 1, 0);
-		data.setComponentAlignment(lastReport, Alignment.TOP_LEFT);
-		data.addComponent(report, 1, 1, 1, 2);
-		data.setComponentAlignment(report, Alignment.TOP_LEFT);
-		data.addComponent(btnNewReport, 1, 3);
-		data.setComponentAlignment(btnNewReport, Alignment.TOP_LEFT);
-		data.addComponent(btnDetails, 1, 4);
-		data.setComponentAlignment(btnDetails, Alignment.TOP_LEFT);
-		data.addComponent(lblBeschriebTitle, 2, 0);
-		data.setComponentAlignment(lblBeschriebTitle, Alignment.TOP_LEFT);
-		data.addComponent(lblBeschrieb, 2, 1, 2, 3);
-		data.setComponentAlignment(lblBeschrieb, Alignment.TOP_LEFT);
-		data.addComponent(btnEmergencyContact, 2, 4);
-		data.setComponentAlignment(btnEmergencyContact, Alignment.TOP_LEFT);
-
-//		data.addComponents(map, checklist);
-
-
-
-
-		// Set the root layout
-		general.addComponent(top);
-		general.addComponent(data);
 
 		return general;
 	}
 
-//	Create pop-up window to enter a new report
+
+
+	//	Create pop-up window to enter a new report
 	private void newReport() {
 		final Window window = new Window("Neuer Rapport erstellen");
 		window.setWidth("90%");
@@ -201,6 +208,7 @@ public class AppointmentDetailView extends BaseView{
 //	clicklistener of button arrival. activate the button "new report" and associate the clicklistener
 	private void btnArrivalClicked(Button btn, Button report){
 		if (!btn.getCaption().equals("Ende bestätigen")) {
+
 			btn.setCaption("Ende bestätigen");
 //			TODO: Persistence arrival time to the db!
 			report.setEnabled(true);

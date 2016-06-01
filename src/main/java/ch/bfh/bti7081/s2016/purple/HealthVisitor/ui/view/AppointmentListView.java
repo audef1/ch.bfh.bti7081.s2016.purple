c714053d00d965ad9dbdd3a3b095d251cf845c61
Florian Auderset <f_auderset@hotmail.com>
2016-06-01 18:29:22 +0200

    added day and weekbuttons

--- src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/view/AppointmentListView.java
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/ui/view/AppointmentListView.java
@@ -10,16 +10,24 @@ import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.HealthVisitorEntity;
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.component.StandardLayout;
 
 import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.event.SelectionEvent;
+import com.vaadin.event.SelectionEvent.SelectionListener;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.components.calendar.CalendarComponentEvents.DateClickEvent;
+import com.vaadin.ui.components.calendar.CalendarComponentEvents.WeekClick;
+import com.vaadin.ui.components.calendar.CalendarComponentEvents.WeekClickHandler;
 import com.vaadin.ui.components.calendar.event.BasicEvent;
+import com.vaadin.ui.components.calendar.handler.BasicDateClickHandler;
 
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.controller.AppointmentListController;
 
@@ -27,6 +35,10 @@ import ch.bfh.bti7081.s2016.purple.HealthVisitor.ui.controller.AppointmentListCo
  * Created by tgdflto1 on 20/05/16.
  */
 public class AppointmentListView extends BaseView {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public static final String NAME = "AppointmentList";
 	public static final String VIEW_NAME = "Terminliste";
 	public static final int FIRST_HOUR = 8;
@@ -64,11 +76,37 @@ public class AppointmentListView extends BaseView {
 				
 		HorizontalLayout calnav = new HorizontalLayout();
 		calnav.setSpacing(true);
+		Calendar cal = new Calendar();
+		
 		Button week = new Button("Woche");
 		Button today = new Button("Heute");
+
+		week.addClickListener(new ClickListener() {
+
+	        private static final long serialVersionUID = 1L;
+
+	        @Override
+	        public void buttonClick(ClickEvent event) {
+	        	GregorianCalendar calendar = new GregorianCalendar();
+	            WeekClickHandler handler = (WeekClickHandler) cal.getHandler(WeekClick.EVENT_ID);
+	            handler.weekClick(new WeekClick(cal, calendar.get(GregorianCalendar.WEEK_OF_YEAR), calendar.get(GregorianCalendar.YEAR)));
+	        }
+	    });
+
+		today.addClickListener(new ClickListener() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+	        public void buttonClick(ClickEvent event) {
+				GregorianCalendar calendar = new GregorianCalendar();
+	            BasicDateClickHandler handler = (BasicDateClickHandler) cal.getHandler(DateClickEvent.EVENT_ID);
+	            handler.dateClick(new DateClickEvent(cal,calendar.getTime()));
+	        }
+	    });
+		
 		calnav.addComponents(week, today);
 		
-		Calendar cal = new Calendar();
 		cal.setReadOnly(true);
 		cal.setSizeFull();
 		cal.setResponsive(true);
@@ -76,6 +114,7 @@ public class AppointmentListView extends BaseView {
 	    cal.setLastVisibleHourOfDay(LAST_HOUR);
 	    cal.setContainerDataSource(container, "caption", "description", "start", "end", "styleName");
 	
+	    
 		general.addComponents(calnav, cal);
 		
 		// TODO Add patients and appointments from db to the list
@@ -88,13 +127,15 @@ public class AppointmentListView extends BaseView {
 		if (items !=null && !items.isEmpty()){
 			for (AppointmentEntity appointment : items) {
 	        	
-				String name = appointment.getClient().getFullName();
+				//String name = appointment.getClient().getFullName();
+				String name = appointment.getPlace();
 	        	String description = appointment.getAddress();
 				Date start = appointment.getStartTime();
 				Date end = appointment.getEndTime();
 				
 				logger.debug("added appointment to calendar: " + name + " - " + start + " - " + end);
-	            container.addBean(new BasicEvent(name, description, start, end));
+	            BasicEvent event = new BasicEvent(name, description, start, end);
+				container.addBean(event);
 	        }
 	        container.sort(new Object[]{"start"}, new boolean[] { true });
         }

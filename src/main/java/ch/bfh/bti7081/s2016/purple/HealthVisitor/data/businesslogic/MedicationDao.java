1311ca2e32998cc8627b2954152dc29e8d085f18
r3h6 <r3h6@outlook.com>
2016-06-10 17:24:14 +0200

    Add medications find by client

--- src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/data/businesslogic/MedicationDao.java
+++ src/main/java/ch/bfh/bti7081/s2016/purple/HealthVisitor/data/businesslogic/MedicationDao.java
@@ -1,9 +1,22 @@
 package ch.bfh.bti7081.s2016.purple.HealthVisitor.data.businesslogic;
 
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Collection;
+
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.entity.AppointmentEntity;
+import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.entity.ClientEntity;
 import ch.bfh.bti7081.s2016.purple.HealthVisitor.data.entity.MedicationEntity;
 
 public class MedicationDao extends GenericDao<MedicationEntity, Integer> {
 
+    private static Logger logger = LogManager.getLogger(MedicationDao.class);
     private static MedicationDao instance;
 
 	public static MedicationDao getInstance(){
@@ -12,4 +25,25 @@ public class MedicationDao extends GenericDao<MedicationEntity, Integer> {
 		}
 		return instance;
 	}
+	
+	public Collection<MedicationEntity> findAllByClient(ClientEntity client){
+    	TypedQuery<MedicationEntity> query = entityManager.createQuery(
+    		"SELECT m "
+    		+ "FROM medication AS m, appointment AS a "
+    		+ "WHERE m.appointment = a.id "
+    		+ "AND a.client = :client",
+    		MedicationEntity.class
+        );
+
+    	Collection<MedicationEntity> medications = null;
+    	try {
+    		medications = query
+                .setParameter("client", client)
+                .getResultList();
+    	} catch (NoResultException e){
+    		logger.debug("No medications found for client " + e.getMessage());
+    	}
+    	return medications;
+	}
+	
 }
